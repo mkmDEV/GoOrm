e9eed8c514afc5cfdfc1dd28b3921c33f826f9bd
mkmdev <mkmdev18@gmail.com>
2019-10-22 15:13:32 +0200

    set up sqlite connection

--- user.go
+++ user.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"net/http"
 )
+
+var db *gorm.DB
+var err error
+
+type User struct {
+	gorm.Model
+	Name  string
+	Email string
+}
+
+func InitialMigration() {
+	db, err = gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Failed to connect to DB")
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&User{})
+}
 
 func AllUsers(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "All Users endpoint hit")
